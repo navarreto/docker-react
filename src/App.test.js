7b10820911c32bf0606b3a593af554eb60aa7029
Navarrete <david.navarretefernandez@atos.net>
2020-10-16 15:01:38 +0200

    fixed test

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders I was Changed on the feature branch link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/I was Changed on the feature branch/i);
   expect(linkElement).toBeInTheDocument();
 });
 
